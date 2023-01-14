09a68ee67514970241a746849770fcd1a6795c29
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-01-14 21:07:22 +0300

    added Task05 and Task07

---
+++ Task05/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 0)
+{
+    int count = -num;
+    while (count <= num)
+    {
+        Console.Write(count + " ");
+        count++;
+    }
+}
---
+++ Task07/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = num % 10;
+Console.WriteLine(res);
