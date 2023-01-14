67a0f3216cebb8a54893e45179f4a1aa98e9f004
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-01-14 19:06:46 +0300

    added Task03

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая на вход принимает два 
+// числа и проверяет, является ли первое число квадратом 
+// второго.
+
+Console.WriteLine("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number2 * number2 == number1)
+{
+    Console.WriteLine("Да");
+} else 
+{
+    Console.WriteLine("Нет");
+}
