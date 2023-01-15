c759dd6ea6ae3eba7a7e39601aa04b2e90c44b60
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-01-15 22:04:36 +0300

    added Task08

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Conslole.ReadLine());
+
+if (num > 0)
+{
+    int count = num;
+    while (count <= num)
+    {
+        if (count % 2 == 0)
+        {
+            Console.WriteLine(count + " ");
+        }
+    count++;
+    }
+}
+else
+{
+    Console.WriteLine("Число должно быть больше нуля");
+}
