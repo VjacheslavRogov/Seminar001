8f9c352d7ebea3b4865f2dcaedaece49c0d141f4
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-01-15 22:18:32 +0300

    completed Task07

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -1,4 +1,14 @@
-﻿Console.WriteLine("Введите число: ");
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает последнюю цифру
+
+Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-int res = num % 10;
-Console.WriteLine(res);
+int res = 0;
+if (num > 99 && num < 1000)
+{
+    res = num % 10;
+    Console.Write($"Последняя цифра равна {res}");
+} else 
+{
+    Console.Write("Число должно быть трёхзначным");
+}
