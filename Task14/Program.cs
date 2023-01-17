1410293a0ff99274fe74be94455ba3a549e75333
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-01-17 22:41:33 +0300

    Complete Task14 and Task16

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -1,2 +1,24 @@
-﻿// Напишите програму, которая принимает на вход 
+﻿// Напишите програму, которая принимает на вход число
 // и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool multiplicity = Multiplicity(num);
+Console.WriteLine(multiplicity ? "Да" : "Нет");
+
+bool Multiplicity(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+
+// int firstCheck = num % 7 == 0 ? firstCheck = 1 : firstCheck = 0;
+    // int secondCheck = num % 23 == 0 ? secondCheck = 1 : secondCheck = 0;
+    // if (firstCheck == 1 && secondCheck == 1)
+    // {
+    //     return true; 
+    // }
+    // else 
+    // {
+    //     return false;
+    // }
--- Task16/Program.cs
+++ Task16/Program.cs
@@ -1,2 +1,16 @@
 ﻿// Напишите программу, которая принимает на вход два числа 
 // и проверяетявляется ли одно число квадратом другого.
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool isSquare = IsSquare(num1, num2);
+
+Console.WriteLine(isSquare ? "Да" : "Нет");
+
+bool IsSquare(int num1, int num2)
+{
+    return num1 * num1 == num2 || num2 * num2 == num1;
+}
