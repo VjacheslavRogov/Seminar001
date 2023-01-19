d0cae9b7de7b620cfced439fcb5ea2dc50bb9e03
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-01-19 22:22:24 +0300

    completed Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -2,12 +2,23 @@
 // и на выходе показывает вторую цифру этого числа. 
 // Выполнить с помощью числовых операций.
 
-int number = new Random().Next(100, 1000);
-Console.WriteLine($"Случайное число из отрезка 100 - 999 -> {number}");
+// int number = new Random().Next(100, 1000);
+// Console.WriteLine($"Случайное число из отрезка 100 - 999 -> {number}");
 
-int result = RemoveSecondDigit(number);
-Console.WriteLine($"{number} -> {result}");
+// int result = RemoveSecondDigit(number);
+// Console.WriteLine($"{number} -> {result}");
 
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    int result = RemoveSecondDigit(number);
+    Console.WriteLine($"{number} -> {result}");
+} else
+{
+    Console.WriteLine("Число не трёхзначное");
+}
 int RemoveSecondDigit(int num)
 {
     int Digit = num % 100;
