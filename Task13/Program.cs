df824421e9fde4de2d62e92853eb17f7d325bb5f
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-01-19 22:45:45 +0300

    completed Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -2,3 +2,23 @@
 // или сообщает, что третьей цифры нет. 
 // Выполнить с помощью числовых операций.
 
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99)
+{
+    while (number > 1000)
+    {
+        number = number / 10;
+    }
+    int thirdDigit = ThirdDigit(number);
+    Console.WriteLine(thirdDigit);
+} else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+int ThirdDigit(int number)
+{
+    return number = number % 10;
+}
