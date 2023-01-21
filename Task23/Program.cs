41ab2cc93d077c9359f57e2351dc09a3906b4dff
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-01-21 14:39:05 +0300

    complete Task23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -3,3 +3,27 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 // Решать как задачу 22
+
+Console.WriteLine("Введите натуральное число: ");
+double number = Convert.ToDouble(Console.ReadLine());
+
+void Cube(double num)
+{
+    double integer = Math.Truncate(num);
+    if (num - integer > 0.0)
+    {
+        num = num * -1;
+    }
+    if (num > 0)
+    {
+        for (int i = 1; i <= num; i++)
+        {
+            Console.WriteLine($"{i,5} -> {i * i * i,5}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введите корректные данные");
+    }
+}
+Cube(number);
