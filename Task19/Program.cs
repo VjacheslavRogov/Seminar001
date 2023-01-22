557de17fa99007d89e46543963d21d1e1dc28525
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-01-22 18:14:08 +0300

    complete Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -4,3 +4,21 @@
 // 23432 -> да
 // 12821 -> да
 // Решать через цикл с разворотом числа
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int turn = Reverse(number);
+string res = (number == turn) ? "Да" : "Нет";
+Console.WriteLine(res);
+
+int Reverse(int num)
+    {
+        int result = 0;
+        while (num != 0)
+        {
+            int remains;
+            num = Math.DivRem(num, 10, out remains);
+            result = result * 10 + remains;
+        }
+        return result;
+    }
