8b3e09a26d5d79b5cbb416fea92d831ccb24c0a5
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-01-25 11:10:20 +0300

    completed Task26

--- Task26/Program.cs
+++ Task26/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 - > 2
+// 89126 -> 5
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int countNumber = CountNumber(number);
+Console.WriteLine($"{number} -> {countNumber}");
+
+int CountNumber(int num)
+{
+    if (num < 0) num = num * -1;
+    int count = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
