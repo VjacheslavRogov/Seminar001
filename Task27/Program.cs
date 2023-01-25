a40729a31b9050e2ba3e59b76f72b6fcabe5ebc2
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-01-25 14:22:15 +0300

    completed Task27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -2,3 +2,21 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+int SumDigit(int num)
+{
+    if (num < 0) num = num * -1;
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10; 
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumDigit = SumDigit(number);
+Console.WriteLine($"Сумма цифр числа {number} равна {sumDigit}");
