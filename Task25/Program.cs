70e007a6c23a91d39dc16dfa1d2bcca43ec654d5
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-01-25 13:30:16 +0300

    сщьздуеу Task25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Exponentiation(int numA, int numB)
+{
+    int result = 1;
+    if (numA < 0) numA = numA * -1;
+    for (int i = 0; i < numB; i++)
+    {
+        result = result * numA;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число A: ");
+int numberA;
+if (int.TryParse(Console.ReadLine(), out numberA))
+{
+    Console.WriteLine("Введите натуральное число B: ");
+    int numberB;
+
+    if (int.TryParse(Console.ReadLine(), out numberB))
+    {
+        if (numberB > 0)
+            Console.WriteLine($"({numberA} в степени {numberB}) -> {Exponentiation(numberA, numberB)}");
+        else
+            Console.WriteLine("Введено некорректное значение. Введите натуральное число B.");
+    }
+    else
+        Console.WriteLine("Введено некорректное значение. Введите натуральное число B.");
+}
+else
+    Console.WriteLine("Введено некорректное значение.");
