6b72ad6dfa2708732ddb3a50351a5dc7c280c496
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-01-30 10:33:51 +0300

    completed Task33

--- Task33/Program.cs
+++ Task33/Program.cs
@@ -3,3 +3,41 @@
 // 4; массив [6, 7, 19, 345, 3] -> нет
 // 3; массив [6, 7, 19, 345, 3] -> да
 
+bool CheckArray(int num, int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == num) return true;
+    }
+    return false;
+}
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i< arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArrayRndInt(5, 0, 6);
+PrintArray(array);
+bool cheked = CheckArray(num, array);
+Console.WriteLine(cheked ? " -> Да" : " -> Нет");
