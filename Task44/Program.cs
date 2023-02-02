a3368bcae576a752da7137aac7de74d8739ad251
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-02-02 18:28:41 +0300

    completed Task44

--- Task44/Program.cs
+++ Task44/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи.
+// Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 5 -> 0 1 1 2 3 5 8
+
+int[] FibonacciArray(int number)
+{
+    int[] fiboArray = new int[number];
+    fiboArray[0] = 0;
+    fiboArray[1] = 1;
+    for (int i = 2; i < number; i++)
+    {
+        fiboArray[i] = fiboArray[i-2] + fiboArray[i-1];
+    }
+    return fiboArray;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+Console.Write("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] result = FibonacciArray(num);
+PrintArray(result);
--- Task45/Program.cs
+++ Task45/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет создавать копию заданного массива 
+// с помощью поэлементного копирования.
+
