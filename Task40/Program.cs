dfbb02b86e8404486e0f6918cfec97e153d0aa16
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-02-02 18:11:26 +0300

    completed Task40 and Task42

--- Task40/Program.cs
+++ Task40/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и 
+// проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: 
+// каждая сторона треугольника меньше суммы двух других сторон.
+
+bool TriangleCheck(int a, int b, int c)
+{
+    return a < (b + c) && b < (a + c) && c < (a + b);
+}
+
+Console.WriteLine("Введите три стороны треугольника.");
+Console.Write("Введите сторону a: ");
+int sideA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите сторону b: ");
+int sideB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите сторону c: ");
+int sideC = Convert.ToInt32(Console.ReadLine());
+
+bool res = TriangleCheck(sideA, sideB, sideC);
+Console.WriteLine(res ? "Да" : "Нет");
--- Task42/Program.cs
+++ Task42/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int NumBinary(int number)
+{
+    int numberBin = 0;
+    int count = 1;
+    while (number > 0)
+    {
+        numberBin = numberBin + (number % 2 * count);
+        number /= 2;
+        count *= 10;
+    }
+    return numberBin;
+}
+
+Console.Write("Введите число: ");
+int num;
+if (int.TryParse(Console.ReadLine(), out num))
+{
+    if (num >= 0)
+    {
+        int result = NumBinary(num);
+        Console.WriteLine(result);
+    }
+    else Console.WriteLine("Введено отрицательное число.");
+}
+else Console.WriteLine("Введено некорректное значение.");
