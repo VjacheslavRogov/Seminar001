412c61a4c0a7c7f0ea2cc42ef925892a8027a889
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-02-10 21:54:01 +0300

    completed Task53 and Task55

--- Task53/Program.cs
+++ Task53/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку массива.
+
+int[,] matrix = CreateMatrixRndInt(4, 5, 0, 255);
+PrintMatrix(matrix);
+Console.WriteLine();
+ReplacementMatrrix(matrix);
+PrintMatrix(matrix);
+
+void ReplacementMatrrix(int[,] matrixMassIn)
+{
+    int temp;
+    for (int i = 0; i < matrixMassIn.GetLength(1); i++)
+    {
+        temp = matrixMassIn[0, i];
+        matrixMassIn[0, i] = matrixMassIn[matrixMassIn.GetLength(0) - 1, i];
+        matrixMassIn[matrixMassIn.GetLength(0) - 1, i] = temp;
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
---
+++ Task55/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задайте двумерный массив. Напишите программу,
+// которая заменит строки на столбцы. В случае, если это невозможно,
+// программа должна вывести сообщение для пользователя.
+
+int[,] matrixGenerate = CreateMatrixRndInt(4, 4, 0, 255);
+PrintMatrix(matrixGenerate);
+Console.WriteLine();
+
+if (IsSquareMatrix(matrixGenerate))
+{
+    PrintMatrix(ReplacementMatrix(matrixGenerate));
+}
+else
+{
+    Console.WriteLine("Невозможно обработать массив!");
+}
+
+bool IsSquareMatrix(int[,] matrix)
+{
+    return matrix.GetLength(0) == matrix.GetLength(1);
+}
+
+int[,] ReplacementMatrix(int[,] matrix)
+{
+    int[,] tempMatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            tempMatrix[j, i] = matrix[i, j];
+        }
+    }
+    return tempMatrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
