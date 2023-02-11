eb7ca530dd9e78aaf868c18d7c1720dfca7af133
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-02-11 09:33:59 +0300

    completed Task54

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -8,3 +8,82 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+void SortToLower(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int UserInput()
+{
+    if (!int.TryParse(Console.ReadLine(), out int temp)) IncorrectValue();
+    return temp;
+}
+
+void IncorrectValue()
+{
+    Console.WriteLine("Введено некорректное значение.");
+    Environment.Exit(0);
+}
+
+Console.WriteLine("Введите кол-во строк в массиве: ");
+int rowsSize = UserInput();
+if (rowsSize <= 0) IncorrectValue();
+Console.WriteLine("Введите кол-во столбцов в массиве: ");
+int columnsSize = UserInput();
+if (columnsSize <= 0) IncorrectValue();
+Console.WriteLine("Введите левую границу диапазона чисел: ");
+int minValue = UserInput();
+Console.WriteLine("Введите правую границу диапазона чисел: ");
+int maxValue = UserInput();
+
+int[,] matrix = CreateMatrixRndInt(rowsSize, columnsSize, minValue, maxValue);
+Console.WriteLine("Заданный массив: ");
+PrintMatrix(matrix);
+Console.WriteLine("");
+Console.WriteLine("Отсортированный массив: ");
+SortToLower(matrix);
+PrintMatrix(matrix);
