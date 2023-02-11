f68c10d05e6ee0f761644c387826837ec6c3932c
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-02-11 09:48:41 +0300

    completed Task56

--- Task56/Program.cs
+++ Task56/Program.cs
@@ -6,3 +6,83 @@
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и 
 // выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+void NumberRowMinSumElements(int[,] array2D)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < array2D.GetLength(1); i++)
+    {
+        minRow += array2D[0, i];
+    }
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++) sumRow += array2D[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{minSumRow + 1} строка");
+}
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int UserInput()
+{
+    if (!int.TryParse(Console.ReadLine(), out int temp)) IncorrectValue();
+    return temp;
+}
+
+void IncorrectValue()
+{
+    Console.WriteLine("Введено некорректное значение.");
+    Environment.Exit(0);
+}
+
+Console.WriteLine("Введите кол-во строк в массиве: ");
+int rowsSize = UserInput();
+if (rowsSize <= 0) IncorrectValue();
+Console.WriteLine("Введите кол-во столбцов в массиве: ");
+int columnsSize = UserInput();
+if (columnsSize <= 0) IncorrectValue();
+Console.WriteLine("Введите левую границу диапазона чисел: ");
+int minValue = UserInput();
+Console.WriteLine("Введите правую границу диапазона чисел: ");
+int maxValue = UserInput();
+
+int[,] matrix = CreateMatrixRndInt(rowsSize, columnsSize, minValue, maxValue);
+Console.WriteLine("Заданный массив: ");
+PrintMatrix(matrix);
+
+NumberRowMinSumElements(matrix);
