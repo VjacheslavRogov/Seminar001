308a3f71d902bb879f2d92ab948c2d22556af41e
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-02-18 10:06:44 +0300

    completed Task66

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -4,3 +4,32 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+void IncorrectValue()
+{
+    Console.WriteLine("Введено некорректное значение.");
+    Environment.Exit(0);
+}
+
+int UserInput()
+{
+    if (!int.TryParse(Console.ReadLine(), out int temp)) IncorrectValue();
+    return temp;
+}
+
+int SumNumbers(int M, int N)
+{
+    if (M == N) return M;
+    else if (M < N) return N + SumNumbers(M, N - 1);
+    else return N + SumNumbers(M, N + 1);
+}
+
+Console.WriteLine("Введите натуральное число M:");
+int numberM = UserInput();
+if (numberM <= 0) IncorrectValue();
+
+Console.WriteLine("Введите натуральное число N:");
+int numberN = UserInput();
+if (numberN <= 0) IncorrectValue();
+
+Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {SumNumbers(numberM, numberN)}");
